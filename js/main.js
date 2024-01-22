0cce9dc815dce964b9c62ac9faaa1dcd0fb29068
alessiarabacchi <alessia.rabacchi@icloud.com>
2024-01-22 15:42:22 +0100

    add interval

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-// # HTML ELEMENTS
 const slidesContainerEl = document.getElementById("slides-container");
 const arrowUpEl = document.querySelector(".arrow-up");
 const arrowDownEl = document.querySelector(".arrow-down");
+
+// Funzione per aggiornare la slide attiva
+function updateSlide(index) {
+  const allSlides = document.getElementsByClassName("slide");
+
+  // Elimino la classe active dalla slide attualmente mostrata
+  const oldSlide = allSlides[slideIndex];
+  oldSlide.classList.remove("active");
+
+  // Mostro la nuova slide
+  slideIndex = index;
+  const newSlide = allSlides[slideIndex];
+  newSlide.classList.add("active");
+}
 
 // # ON LOAD
 // * definisco l'Array delle slide
@@ -14,7 +27,7 @@ let slideIndex = 0;
 let slidesHtml = "";
 for (let i = 0; i < slides.length; i++) {
   const slide = slides[i];
-  let activeClass = i == slideIndex ? "active" : "";
+  let activeClass = i === slideIndex ? "active" : "";
 
   slidesHtml += `<img src="./img/${slide}" class="slide ${activeClass}" alt="slide ${i}">`;
 }
@@ -23,42 +36,18 @@ slidesContainerEl.innerHTML = slidesHtml;
 
 // # CLICK ARROW UP
 arrowUpEl.addEventListener("click", function () {
-  //* recupero tutte le slide
-  const allSlides = document.getElementsByClassName("slide");
-
-  //* elimino la classe active dalla slide attualmente mostrata
-  const oldSlide = allSlides[slideIndex];
-  oldSlide.classList.remove("active");
-
-  //* incremento slideIndex
-  if (slideIndex >= allSlides.lenght - 1) {
-    slideIndex = 0;
-  } else {
-    slideIndex++;
-  }
-
-  //* mostro la nuova slide
-  const newSlide = allSlides[slideIndex];
-  newSlide - classList.add("active");
+  const newIndex = slideIndex === 0 ? slides.length - 1 : slideIndex - 1;
+  updateSlide(newIndex);
 });
 
 // # CLICK ARROW DOWN
 arrowDownEl.addEventListener("click", function () {
-  //* recupero tutte le slide
-  const allSlides = document.getElementsByClassName("slide");
-
-  //* elimino la classe active dalla slide attualmente mostrata
-  const oldSlide = allSlides[slideIndex];
-  oldSlide.classList.remove("active");
-
-  //* decremento slideIndex
-  if (slideIndex <= 0) {
-    slideIndex = allSlides.length - 1;
-  } else {
-    slideIndex--;
-  }
-
-  //* mostro la nuova slide
-  const newSlide = allSlides[slideIndex];
-  newSlide - classList.add("active");
+  const newIndex = (slideIndex + 1) % slides.length;
+  updateSlide(newIndex);
 });
+
+// Impostazione dell'intervallo per lo slider automatico ogni 3 secondi
+setInterval(function () {
+  const newIndex = (slideIndex + 1) % slides.length;
+  updateSlide(newIndex);
+}, 3000);
